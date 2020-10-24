0393cb89eac9876e269a5a3c7aa7aaf1c5e8b56e
Artur Szatkowski <szatkowski.artur@gmail.com>
2020-10-24 13:06:56 +0200

    ADD: simple price calculator from yt tutorial.

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var salesTxt: UITextField!
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        totalPriceLbl.text = "";
     }
 
-
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        let price = Double(priceTxt.text!)!;
+        let salesTax = Double(salesTxt.text!)!;
+        let totalSalesTax = price * salesTax;
+        
+        let totalPrice = price * totalSalesTax;
+        totalPriceLbl.text = "$\(totalPrice)";
+    }
+    
 }
 
